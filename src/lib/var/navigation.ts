a9d85d9865b304a458fcf397a9e84e48fa6b290f
jsouthgb <22480336+JSouthGB@users.noreply.github.com>
2025-07-01 07:27:36 -0400

    refactor: remove unused /options route

--- src/lib/var/navigation.ts
+++ src/lib/var/navigation.ts
@@ -60,12 +60,6 @@ export const navItems = [
         icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path d="M13 10h28v34H13z" /><path stroke-linecap="round" d="M35 10V4H8a1 1 0 0 0-1 1v33h6m8-16h12m-12 8h12" /></g></svg>`,
         href: '/settings/debug',
       },
-      {
-        id: 'options',
-        label: 'Options Manager',
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path d="M13 10h28v34H13z" /><path stroke-linecap="round" d="M35 10V4H8a1 1 0 0 0-1 1v33h6m8-16h12m-12 8h12" /></g></svg>`,
-        href: '/settings/options',
-      },
     ],
   },
 ];
