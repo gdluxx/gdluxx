bf2d3c99dbc14ced0c59337002ac1d3f5deecbcf
jsouthgb <22480336+JSouthGB@users.noreply.github.com>
2025-08-12 20:17:49 -0400

    chore: add copyright to extension files

--- extension/components/counter.ts
+++ extension/components/counter.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 jsouthgb
+ *
+ * This file is part of gdluxx.
+ *
+ * gdluxx is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 (GPL-2.0),
+ * as published by the Free Software Foundation.
+ */
+
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0;
   const setCounter = (count: number) => {
--- extension/entrypoints/popup/main.ts
+++ extension/entrypoints/popup/main.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 jsouthgb
+ *
+ * This file is part of gdluxx.
+ *
+ * gdluxx is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 (GPL-2.0),
+ * as published by the Free Software Foundation.
+ */
+
 import browser, { Tabs } from 'webextension-polyfill';
 import './style.css';
 import Tab = Tabs.Tab;
